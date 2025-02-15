136945ea94c34117058fc1da7e2731a21d9432cb
CALLIA <alexsandrralexsandr@gmail.com>
2025-02-15 22:11:49 +0300

    Norm settings

--- settings.py
+++ settings.py
@@ -1,20 +1,20 @@
 typeRus = {
-    'Attaker': 'Атакер', 'Defender': 'Защитник', 'Shoter': 'Стрелок'
+    'Attaker': 'Атакер', 'Defender': 'Защитник', 'Shooter': 'Стрелок'
 }
 typeHP = {
-    'Attaker': 50, 'Defender': 140, 'Shoter': 30
+    'Attaker': 50, 'Defender': 140, 'Shooter': 30
 }
 typeDamag = {
-    'Attaker': 40, 'Defender': 20, 'Shoter': 40
+    'Attaker': 40, 'Defender': 20, 'Shooter': 40
 }
 typeDist = {
-    'Attaker': 20, 'Defender': 20, 'Shoter': 50
+    'Attaker': 20, 'Defender': 20, 'Shooter': 50
 }
 typeCost = {
-    'Attaker': 10, 'Defender': 50, 'Shoter': 50
+    'Attaker': 10, 'Defender': 50, 'Shooter': 50
 }
 typeColors = {
     'Attaker': (255, 165, 0),
     'Defender': (0, 128, 128),
-    'Shoter': (0, 255, 0)
+    'Shooter': (0, 255, 0)
 }
