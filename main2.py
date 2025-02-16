d5982a6bc00e9cbc18e4eeb04ba41de41f0de72d
Andrew Frolov <Frolov68oa@gmail.com>
2025-02-17 00:26:47 +0300

    igra

--- main2.py
+++ main2.py
@@ -1,16 +1,21 @@
 import pygame
 import sys
 import random
-from settings import *
 
 pygame.init()
 WIDTH, HEIGHT = 1200, 600
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-
 pygame.display.set_caption("Primitives War")
+
+starting_coins = 1000
+current_coins = starting_coins
+player_HP = 100
 
 units = []
 enemies = []
+bullets = []
+
+shooters_last_shoot_time = {}
 
 typeHP = {
     'Attaker': 50,
@@ -18,6 +23,11 @@ typeHP = {
     'Shooter': 100
 }
 
+typeCost = {
+    'Attaker': 10,
+    'Defender': 50,
+    'Shooter': 50
+}
 
 class Unit:
     def __init__(self, type, x, y):
@@ -45,6 +55,10 @@ class Unit:
                 self.y < enemy.y + enemy.size[1] and
                 self.y + self.size[1] > enemy.y)
 
+    def shoot(self):
+        if self.type == 'Shooter':
+            bullet = Bullet(self.x + self.size[0], self.y + (self.size[1] // 2))
+            bullets.append(bullet)
 
 class Enemy:
     def __init__(self, x, y):
@@ -59,22 +73,31 @@ class Enemy:
     def update(self):
         self.x -= 1
 
+class Bullet:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.size = (10, 5)
+        self.color = (255, 215, 0)
+
+    def draw(self, screen):
+        pygame.draw.rect(screen, self.color, (self.x, self.y, *self.size))
+
+    def update(self):
+        self.x += 5
 
 def spawn_enemy():
     y = random.randint(72, HEIGHT - 50)
     new_enemy = Enemy(WIDTH, y)
     enemies.append(new_enemy)
 
-
 class Button:
-    def __init__(self, text, x, y, width, height, color, hover_color, action=None, argv=None, about=None):
+    def __init__(self, text, x, y, width, height, color, hover_color, action=None):
         self.text = text
         self.rect = pygame.Rect(x, y, width, height)
         self.color = color
         self.hover_color = hover_color
         self.action = action
-        self.argv = argv
-        self.about = about
 
     def draw(self, screen):
         mouse_pos = pygame.mouse.get_pos()
@@ -91,111 +114,62 @@ class Button:
         if event.type == pygame.MOUSEBUTTONDOWN:
             if self.rect.collidepoint(event.pos):
                 if self.action:
-                    if self.argv:
-                        self.action(self.argv)
-                    else:
-                        self.action()
-
-    def draw2(self, screen):
-        mouse_pos = pygame.mouse.get_pos()
-        if self.about and self.rect.collidepoint(mouse_pos):
-            lines = self.about.splitlines()
-            count = len(lines)
-            maxLen = max(len(x) for x in lines)
-            width = 10 * maxLen
-            height = 25 * count
-            LineX, LineY = self.rect.centerx, self.rect.bottom + 10
-
-            pygame.draw.rect(screen, (255, 255, 255), (LineX - width // 2 - 10, LineY, width + 20, height + 10))
-            pygame.draw.rect(screen, (0, 0, 0), (LineX - width // 2 - 10, LineY, width + 20, height + 10), 2)
-
-            font = pygame.font.Font(None, 25)
-            for i, line in enumerate(lines):
-                text_surface = font.render(line, True, 'black')
-                text_rect = text_surface.get_rect(center=(LineX, LineY + (i * 25) + 5))
-                screen.blit(text_surface, text_rect)
-
+                    self.action()
 
 def spawn_unit(x, y):
-    global currType
+    global currType, current_coins
     if currType != 'None' and x < (WIDTH // 2 - 45):
-        new_unit = Unit(currType, x, y)
-        units.append(new_unit)
+        unit_cost = typeCost[currType]
+        if current_coins >= unit_cost:
+            new_unit = Unit(currType, x, y)
+            units.append(new_unit)
+            current_coins -= unit_cost
 
-
-def handle_event(event, buttons: list):
+def handle_event(event, buttons):
     for butt in buttons:
         butt.handle_event(event)
-
     if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
         x, y = event.pos
         if y > 72:
             spawn_unit(x, y)
 
-
-def drower(screen, objs):
+def draw_objects(screen, objs):
     for obj in objs:
         obj.draw(screen)
-        obj.draw2(screen)
 
-    for unit in units:
-        unit.draw(screen)
-
-    for enemy in enemies:
-        enemy.draw(screen)
-
-
-enemy_spawn_time = 0
-start = False
-
-
-def startGame():
-    global startBtn, start, typeLabel, currType
-    if not start:
-        start = True
-        typeLabel = 'Lock'
-        currType = 'None'
-        startBtn.text = 'Заново'
-        startBtn.color = (255, 0, 0)
-        startBtn.hover_color = (255 - 25, 0, 0)
-    else:
-        start = False
-        typeLabel = 'None'
-        currType = 'None'
-        startBtn.text = 'Готов!'
-        startBtn.color = (128, 0, 128)
-        startBtn.hover_color = (128 - 20, 0, 128 - 20)
-
-
-def setClass(cl):
-    global currType, typeLabel
-    if currType != cl:
-        currType = cl
-        typeLabel = typeRus[cl]
-    else:
-        currType = 'None'
-        typeLabel = 'None'
-
+def reset_game():
+    global currType, current_coins, player_HP, units, enemies
+    currType = 'None'
+    current_coins = starting_coins
+    player_HP = 100
+    units.clear()
+    enemies.clear()
+    shooters_last_shoot_time.clear()
 
 running = True
-typeLabel = 'None'
 currType = 'None'
-startBtn = Button('Готов!', 0, 20, 100, 50, (128, 0, 128),
-                  (128 - 20, 0, 128 - 20), startGame)
+enemy_spawn_time = 0
+shoot_interval = 5 * 1000
 
-classAttak = Button('Атакер', 100, 20, 100, 50, (255, 165, 0),
-                    (240, 150, 0), setClass, ('Attaker'),
-                    f'Урон {typeDamag["Attaker"]}\nОЗ {typeHP["Attaker"]}\nСтоит {typeCost["Attaker"]}$\nБлижний бой')
-classDefender = Button('Защитник', 200, 20, 150, 50, (0, 128, 128),
-                       (0, 128 - 15, 128 - 15), setClass, ('Defender'),
-                       f'Урон {typeDamag["Defender"]}\nОЗ {typeHP["Defender"]}\nСтоит {typeCost["Defender"]}$\nБлижний бой')
-classShooter = Button('Стрелок', 350, 20, 110, 50, (0, 255, 0),
-                      (0, 255 - 25, 0), setClass, ('Shooter'),
-                      f'Урон {typeDamag["Shooter"]}\nОЗ {typeHP["Shooter"]}\nСтоит {typeCost["Shooter"]}$\nДальний бой')
+startBtn = Button('Ready!', 0, 20, 100, 50, (128, 0, 128),
+                  (128 - 20, 0, 128 - 20), reset_game)
+
+classAttak = Button('Attacker', 100, 20, 100, 50, (255, 165, 0),
+                    (240, 150, 0), lambda: setClass('Attaker'))
+classDefender = Button('Defender', 200, 20, 150, 50, (0, 128, 128),
+                       (0, 128 - 15, 128 - 15), lambda: setClass('Defender'))
+classShooter = Button('Shooter', 350, 20, 110, 50, (0, 255, 0),
+                      (0, 255 - 25, 0), lambda: setClass('Shooter'))
 
 buttons = [startBtn, classAttak, classDefender, classShooter]
-
 damage_on_collision = 10
+
+def setClass(cl):
+    global currType
+    if currType != cl:
+        currType = cl
+    else:
+        currType = 'None'
 
 while running:
     for event in pygame.event.get():
@@ -205,45 +179,67 @@ while running:
 
     screen.fill((255, 255, 255))
 
+    enemy_spawn_time += 1
     if enemy_spawn_time > 30:
         spawn_enemy()
         enemy_spawn_time = 0
 
-    for enemy in enemies:
+    for enemy in enemies[:]:
         enemy.update()
+        if enemy.x < 0:
+            player_HP -= 1
+            enemies.remove(enemy)
 
-    for unit in units:
-        for enemy in enemies:
+    for unit in units[:]:
+        for enemy in enemies[:]:
             if unit.is_colliding(enemy):
                 unit.HP -= damage_on_collision
                 if unit.HP <= 0:
                     units.remove(unit)
                 enemies.remove(enemy)
+                current_coins += 10
                 break
 
-    enemies = [enemy for enemy in enemies if enemy.x > 0]
+    current_time = pygame.time.get_ticks()
+    for unit in units:
+        if unit.type == 'Shooter':
+            if (unit.x not in shooters_last_shoot_time or
+                    current_time - shooters_last_shoot_time[unit.x] > shoot_interval):
+                unit.shoot()
+                shooters_last_shoot_time[unit.x] = current_time
 
-    drower(screen, buttons)
+    bullets[:] = [bullet for bullet in bullets if bullet.x < WIDTH]
+    for bullet in bullets:
+        bullet.update()
+
+    for bullet in bullets[:]:
+        for enemy in enemies[:]:
+            if bullet.x + bullet.size[0] > enemy.x and bullet.y + bullet.size[1] > enemy.y and bullet.y < enemy.y + \
+                    enemy.size[1]:
+                bullets.remove(bullet)
+                enemies.remove(enemy)
+                current_coins += 10
+                break
+
+    draw_objects(screen, buttons)
+    draw_objects(screen, units)
+    draw_objects(screen, enemies)
+    draw_objects(screen, bullets)
 
     font = pygame.font.Font(None, 36)
-    if typeLabel == 'None':
-        text_surface = font.render('Классы', True, 'black')
-    elif typeLabel == 'Lock':
-        text_surface = font.render('Идет бой', True, 'black')
-    else:
-        text_surface = font.render(f'Выбран: {typeLabel}', True, 'black')
-    text_rect = text_surface.get_rect(center=((100 + 350 + 110) // 2, 10))
+    hp_text = f"HP: {player_HP}"
+    text_surface = font.render(hp_text, True, 'red')
+    text_rect = text_surface.get_rect(topright=(WIDTH - 10, 10))
     screen.blit(text_surface, text_rect)
 
-    pygame.draw.line(screen, (0, 0, 0), (0, 72), (WIDTH, 72), 1)
-    pygame.draw.line(screen, (128, 0, 0), (WIDTH / 2, 72), (WIDTH / 2, HEIGHT), 5)
+    coins_text = f"Coins: {current_coins}"
+    coins_surface = font.render(coins_text, True, 'gold')
+    coins_rect = coins_surface.get_rect(topright=(WIDTH - 10, 40))
+    screen.blit(coins_surface, coins_rect)
 
-    font = pygame.font.Font(None, 50)
-    coinsText = f"10^9 $"
-    text_surface = font.render(coinsText, True, 'gold')
-    text_rect = text_surface.get_rect(center=(WIDTH - (10 * len(coinsText)), 20))
-    screen.blit(text_surface, text_rect)
+    if player_HP <= 0:
+        print("Game Over!")
+        running = False
 
     pygame.display.flip()
 
-    enemy_spawn_time += 1
